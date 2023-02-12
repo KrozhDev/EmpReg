5bbc66ff98b5f40192fca00a0866f0e9ae2297f5
KrozhDev <roko.mdn@gmail.com>
2023-02-12 12:44:55 +0300

    Main and Readme

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] sales = new int[] {4,3,1,5,3,7,5,1};
+        SalesManager sm = new SalesManager(sales);
+        System.out.println(sm.max());
+    }
+}
